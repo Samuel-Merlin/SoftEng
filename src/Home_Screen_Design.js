f8d71f53e44e517886b3d273f0dae0d27cfde52f
Sam M <samuel.merlin@stvincent.edu>
2023-03-14 19:27:37 -0400

    AppBuild

--- src/Home_Screen_Design.js
+++ src/Home_Screen_Design.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import cal_480 from './IMG/cal_480.png';
+import CalApp from './Images/logo.png';
+import './login.css'
 function Home_Screen()
 {
       return(
-        <div>
-          <Link to='/PersonalCalendar'>Link</Link>
+        <div className='body'>
+            <div className='Header'> MyCalendarTime.com </div>
+                <div className='ItemContainer'>
+                  <div className='ImageContainer'>
+                    <img className='bigImg' src={CalApp} alt='sd'/>
+                    <img className='bigImg' src={CalApp} alt='sd'/>
+                </div>
+                  <div className='link'>
+                    <Link to='/PersonalCalendar'>Personal Calendar</Link>
+                      </div>
+                      
+                    <div className='link'>  
+                      <Link to='/PersonalCalendar'>Group Calendar</Link>
+                </div>
+              </div>
         </div>
-                 
+               //<img className='bigImg' src={CalApp} alt='sd'/>  
 );}
 export default Home_Screen;
--- src/Temp.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function Cal() {
-    return (
-        <div>pooopoooo</div>
-    )
-  }
-export default Cal

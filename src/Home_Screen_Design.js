f22ae1c45906b5e8fb0fdca277099485d97f9a9d
mcdonagp <126005690+mcdonagp@users.noreply.github.com>
2023-03-18 16:54:34 -0400

    Home Screen Design

--- src/Home_Screen_Design.js
+++ src/Home_Screen_Design.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import CalApp from './Images/logo.png';
+import halfLogo from './Images/halfLogo.png';
 import './login.css'
+import './JBstyles.css'
 function Home_Screen()
 {
       return(
         <div className='body'>
-            <div className='Header'> MyCalendarTime.com </div>
+               <header>
+       <div className="flexbox-container">
+         <div className="flexbox-item flexbox-item-1"> <img className='header' src={halfLogo} alt='sd'/>
+         <br></br>
+          </div>
+       </div>
+     </header>
                 <div className='ItemContainer'>
                   <div className='ImageContainer'>
                     <img className='bigImg' src={CalApp} alt='sd'/>

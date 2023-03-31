8abd75b1401ba8ddaaaa96d83f35c6de3cf0f981
mcdonagp <126005690+mcdonagp@users.noreply.github.com>
2023-03-31 15:24:18 -0400

    Update addTaskForm.js

--- src/addTaskForm.js
+++ src/addTaskForm.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.css';
-
+import { oFirestore } from './firebase-config';
+import {collection,getDocs,addDoc} from 'firebase/firestore';
+import moment from 'moment';
 import { NavLink, useNavigate, Link } from 'react-router-dom'
 
 function TaskDesign()
 {
   const navigate = useNavigate();
+  const [evName, setevName] = useState([])
+  const [evTimestamp, setevTimestamp] = useState([])
+  async function handleSubmit(){
+    const Message = document.getElementById('Message');
+    if (evName =='' || evTimestamp == ''){
+      Message.innerHTML = "Invalid input, please enter all form elements";
+    }
+    else{
+    try{
+    const newDocRef = await addDoc(collection(oFirestore, 'GCEvents'),{
+        event_name: evName,
+        event_timestamp: new Date(evTimestamp)
+    });
+    const Message = document.getElementById('Message');
+      Message.innerHTML = "Event succesfully created";
+      document.querySelector(".dateNtime").value = "";
+      document.querySelector(".inputText").value = "";
+      setevName("");
+      setevTimestamp("");
+    }
+    catch(error) {
+        console.log(error);
+        const Message = document.getElementById('Message');
+        Message.innerHTML = "Invalid input, please enter all form elements";
+    };
+  }
+}
 return(
+
   
   <div className='body'>
             <div className='Header'> MyCalendarTime.com </div>
@@ -15,14 +45,16 @@ return(
         <div className='ItemContainer'>
         <div className='Header2'> Add Task Form </div>
         <div className='date'> Select a Date and Time</div>
-        <div>
-        <input value='value' className="dateNtime" type="datetime-local" id="add an ID"></input>
-        </div>
+        <input defualtValue='value' className="dateNtime" type="datetime-local" id="add an ID" onChange={(event) => {
+                        setevTimestamp(event.target.value);}}></input>
 
         <div className='title'> Title and Description </div>
         <div>
         <input className='inputText'
+        defualtValue='value'
           placeholder="Add text here"
+          onChange={(event) => {
+            setevName(event.target.value);}}
         />
       </div>
       <div className='txtArea'>
@@ -36,9 +68,10 @@ return(
         </div>
         <button className='byeuton1' onClick={() => navigate('/PersonalCalendar')}> Go Back</button>
         &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-        <button className='byeuton2' onClick={() => navigate('/PersonalCalendar')}> Submit</button>
+        <button className='byeuton2' onClick={handleSubmit}> Submit</button>
                     </div>
                     </div>
-             </div>
+                    <p id ='Message'></p>
+      </div>
 );}
 export default TaskDesign;
